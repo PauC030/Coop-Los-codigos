b8903217fe974cf158f24a69ad1be41fb059ab17
Andreaa <paulaandrea30.x@gmail.com>
2024-09-05 07:59:16 -0500

    actualizado modo de tema en la pagina principal

---
+++ index.js
@@ -0,0 +1,15 @@
+function toggleTheme() {
+    const body = document.body;
+    const themeIcon = document.getElementById('theme-icon');
+    
+    body.classList.toggle('dark-mode');
+    
+    // Update icon based on theme
+    if (body.classList.contains('dark-mode')) {
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
+    } else {
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
+    }
+}
